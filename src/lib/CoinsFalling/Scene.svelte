51ff26108855945bbe58acd92f99d94c69f974eb
JollyGrin <lol@dean.lol>
2025-03-06 17:37:49 +0100

    update

--- src/lib/CoinsFalling/Scene.svelte
+++ src/lib/CoinsFalling/Scene.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
-	import { T } from '@threlte/core';
+	import { T, useTask } from '@threlte/core';
 	import { OrbitControls, AudioListener } from '@threlte/extras';
 	import Emitter from './Emitter.svelte';
 	import Ground from './Ground.svelte';
 </script>
 
-<T.PerspectiveCamera makeDefault position={[12, 8, 12]} fov={45}>
-	<OrbitControls 
+<T.PerspectiveCamera makeDefault position={[0, 2, 12]} fov={45}>
+	<OrbitControls
+		autoRotate
+		rotateSpeed={0.1}
 		enableZoom
 		minDistance={10}
-		maxDistance={20}
-		target={[0, 0, 0]} 
-		maxPolarAngle={Math.PI * 0.6}
+		maxDistance={25}
+		target={[0, 0, 0]}
+		maxPolarAngle={Math.PI * 0.5}
 		minPolarAngle={Math.PI * 0.1}
 		enableDamping
 		dampingFactor={0.05}
@@ -20,7 +22,7 @@
 </T.PerspectiveCamera>
 
 <!-- Main Light -->
-<T.DirectionalLight castShadow position={[10, 15, 5]} intensity={1.2}>
+<T.DirectionalLight castShadow position={[10, 15, 5]} intensity={3}>
 	<T.OrthographicCamera attach="shadow-camera" args={[-10, 10, 10, -10, 0.1, 50]} />
 </T.DirectionalLight>
 
@@ -28,7 +30,7 @@
 <T.DirectionalLight position={[-5, 10, -5]} intensity={0.4} />
 
 <!-- Ambient Light -->
-<T.AmbientLight intensity={0.4} />
+<T.AmbientLight intensity={1} />
 
 <Ground />
 
